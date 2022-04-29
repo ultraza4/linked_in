137e373506fbc07e6d9f0ad2a9b45999389689b4
Azeke Work <aza_kz14@mail.ru>
2022-04-29 16:43:43 +0600

    refactured NetworkPage with class component and componentDidMount

--- src/components/NetworkPage/NetworkPage.jsx
+++ src/components/NetworkPage/NetworkPage.jsx
@@ -3,47 +3,43 @@ import * as axios from "axios";
 import s from "./NetworkPage.module.css"
 import userPhoto from "../../assets/images/user.png.png";
 
-const NetworkPage = (props) => {
-    let usersGetter = () => {
-        if (props.users.length == 0) {   
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                    props.setUsers(response.data.items)
-            })
-                // { id: 1, subscribed: true, fullName: "Kagarman A.N", status: "Hello World Bitches", location: { country: "Kazakhstan", city: "Oskemen" } },
-                // { id: 2, subscribed: true, fullName: "Rizabekov D.", status: "Hello World Bitches", location: { country: "Kazakhstan", city: "Almaty" } },
-                // { id: 3, subscribed: false, fullName: "Sarsembaev R.", status: "Hello World Bitches", location: { country: "Kazakhstan", city: "Astana" } },
-        }
+class NetworkPage extends React.Component {
+   
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
     
-
-    return <div>
-        <button onClick={usersGetter}>Get Users</button>
-        {
-            props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img className={s.user_avatar} src={u.photos.small != null ? u.photos.small : userPhoto }/>
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={() => {
-                                props.unfollow(u.id)
-                            }}>Unfollow</button>
-                            : <button onClick={() => {
-                                props.follow(u.id)
-                            }}>Follow</button>}
-
-                    </div>
-                </span>
-                <span>
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
                     <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
+                        <div>
+                            <img className={s.user_avatar} src={u.photos.small != null ? u.photos.small : userPhoto} />
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => {
+                                    this.props.unfollow(u.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    this.props.follow(u.id)
+                                }}>Follow</button>}
+
+                        </div>
                     </span>
-                </span>
-            </div>)
-        }
-    </div>
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                    </span>
+                </div>)
+            }
+        </div>
+    }
 }
 
 export default NetworkPage;
