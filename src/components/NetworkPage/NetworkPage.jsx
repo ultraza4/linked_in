1240ae34eccc146fafe703b33d3a8ad6effd69f0
Azeke Home <helloDude@gmail.com>
2022-04-30 23:33:16 +0600

    moved file NetworkPageAPI to NetworkPageContainer as class

--- src/components/NetworkPage/NetworkPage.jsx
+++ src/components/NetworkPage/NetworkPage.jsx
@@ -1,65 +1,49 @@
 import React from "react";
-import * as axios from "axios";
 import s from "./NetworkPage.module.css"
 import userPhoto from "../../assets/images/user.png.png";
 
-class NetworkPage extends React.Component {
-   
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.activePageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-        })
-    }
-    onPageChanged =(pageNumber) => {
-        this.props.setActivePage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-        this.props.setUsers(response.data.items);
-    })
-    }
+let NetworkPage = (props) => {
 
-    render() {
+   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+   let pages = [];
 
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-        
-        for(let i = 1;i <= pagesCount; i++ ){
-            pages.push(i);
-        }
-        
-        return <div>
-            {pages.map( p => {
-                return <span className={this.props.activePageNumber === p && s.activePage} 
-                onClick= {(e) => this.onPageChanged(p)}> {p} </span>
-            })}
+   for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+   }
 
-            {
-                this.props.users.map(u => <div key={u.id}>
-                    <span>
-                        <div>
-                            <img className={s.user_avatar} src={u.photos.small != null ? u.photos.small : userPhoto} />
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => {
-                                    this.props.unfollow(u.id)
-                                }}>Unfollow</button>
-                                : <button onClick={() => {
-                                    this.props.follow(u.id)
-                                }}>Follow</button>}
+   return <div>
+      {pages.map(p => {
+         return <span className={props.activePageNumber === p && s.activePage}
+            onClick={(e) => props.onPageChanged(p)}> {p} </span>
+      })}
 
-                        </div>
-                    </span>
-                    <span>
-                        <span>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
-                        </span>
-                    </span>
-                </div>)
-            }
-        </div>
-    }
+      {
+         props.users.map(u => <div key={u.id}>
+            <span>
+               <div>
+                  <img className={s.user_avatar} src={u.photos.small != null ? u.photos.small : userPhoto} />
+               </div>
+               <div>
+                  {u.followed
+                     ? <button onClick={() => {
+                        props.unfollow(u.id)
+                     }}>Unfollow</button>
+                     : <button onClick={() => {
+                        props.follow(u.id)
+                     }}>Follow</button>}
+
+               </div>
+            </span>
+            <span>
+               <span>
+                  <div>{u.name}</div>
+                  <div>{u.status}</div>
+               </span>
+            </span>
+         </div>)
+      }
+   </div>
 }
+
 
 export default NetworkPage;
--- src/components/NetworkPage/NetworkPageContainer.jsx
+++ src/components/NetworkPage/NetworkPageContainer.jsx
@@ -1,6 +1,35 @@
+import React from "react";
 import { connect } from "react-redux";
 import { followAC, unfollowAC, setUsersAC, setActivePageAC, setTotalUsersCountAC } from "./../../redux/NetworkPageReducer"
+import * as axios from "axios";
 import NetworkPage from "./NetworkPage";
+
+class NetworkPageAPI extends React.Component {
+
+   componentDidMount() {
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.activePageNumber}&count=${this.props.pageSize}`).then(response => {
+         this.props.setUsers(response.data.items);
+         this.props.setTotalUsersCount(response.data.totalCount);
+      })
+   }
+   onPageChanged = (pageNumber) => {
+      this.props.setActivePage(pageNumber);
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+         this.props.setUsers(response.data.items);
+      })
+   }
+
+   render() {
+      return <NetworkPage activePageNumber={this.props.activePageNumber}
+         totalUsersCount={this.props.totalUsersCount}
+         pageSize={this.props.pageSize}
+         users={this.props.users}
+         onPageChanged={this.onPageChanged}
+         follow={this.props.follow}
+         unfollow={this.props.unfollow}
+      />
+   }
+}
 
 const mapStateToProps = (state) => {
    return {
@@ -21,6 +50,6 @@ const mapDispatchToProps = (dispatch) => {
    }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NetworkPage);
+export default connect(mapStateToProps, mapDispatchToProps)(NetworkPageAPI);
 
 
